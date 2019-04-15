f7bec66d6b588ee90ebfac797d25b23801231b1f
Aluno <aluno@eteclencois.com.br>
2019-04-15 11:43:10 -0300

    Aula API UI 15/04

--- src/app/categorias/categoria.service.ts
+++ src/app/categorias/categoria.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+export class CategoriaFiltro{
+  nome: string;
+  pagina = 0;
+  itensporpagina = 2;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +15,30 @@ export class CategoriaService {
   constructor(private http: HttpClient) { }
 
   categoriasUrl = 'http://localhost:8080/categorias';
+
+  pesquisar(filtro: CategoriaFiltro):Promise<any>{
+      let params = new HttpParams({
+        fromObject: {
+          page: filtro.pagina.toString();
+          size: filtro.itensporpagina.toString()
+        }
+      });
+
+      if (filtro.nome) {
+        params = params.append('nome', filtro.nome);
+      }
+
+      return this.http.get<any>(`${this.categoriasUrl}`, {params})
+      .toPromise()
+      .then(response =>{
+        const categorias = response.content;
+
+        const resultado = {
+          categorias,
+          total: response.totalElements;
+        }
+
+        return resultado;
+      });
+  }
 }
